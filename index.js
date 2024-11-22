5ee93d37485ee70e65988f5eb692a20b03555d8d
Mathias Thierbach <mt-dev@outlook.com>
2024-11-22 17:46:44 -0600

    Add "Workloads" section

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const yaml = require('js-yaml');
 
 // Define the directory path
 const svgDir = path.join(__dirname, 'node_modules', '@fabric-msft', 'svg-icons', 'dist', 'svg');
 const readmePath = path.join(__dirname, 'README.md');
 const packageJsonPath = path.join(__dirname, 'node_modules', '@fabric-msft', 'svg-icons', 'package.json');
+const configPath = path.join(__dirname, 'config.yml');
 
 // Function to filter files based on a pattern
 const filterFiles = (files, pattern) => {
@@ -27,6 +29,16 @@ const groupFilesByBaseName = (files) => {
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 const version = packageJson.version;
 
+// Read and parse the config.yml file
+let workloads = [];
+try {
+  const configFile = fs.readFileSync(configPath, 'utf8');
+  const config = yaml.load(configFile);
+  workloads = config.workloads || [];
+} catch (e) {
+  console.error('Error reading config.yml:', e);
+}
+
 // Read the directory contents
 fs.readdir(svgDir, (err, files) => {
   if (err) {
@@ -48,10 +60,21 @@ fs.readdir(svgDir, (err, files) => {
   writeStream.write(`# fabric-icons (${version})\n\n`);
 
   // Function to write grouped files to README.md
-  const writeGroupedFiles = (groupedFiles, header) => {
+  const writeGroupedFiles = (groupedFiles, header, include = [], exclude = []) => {
+    if (include && include.length > 0 && exclude && exclude.length > 0) {
+      throw new Error('Cannot provide both include and exclude arrays.');
+    }
+
     writeStream.write(`## ${header}\n\n`);
 
     Object.entries(groupedFiles).forEach(([baseName, files]) => {
+      if (include && include.length > 0 && !include.includes(baseName)) {
+        return;
+      }
+      if (exclude && exclude.length > 0 && exclude.includes(baseName)) {
+        return;
+      }
+
       console.log(`Processing ${baseName}...`);
       writeStream.write(`### ${baseName}\n\n`);
 
@@ -66,8 +89,9 @@ fs.readdir(svgDir, (err, files) => {
   };
 
   // Write grouped item files and other files to README.md
+  writeGroupedFiles(groupedOtherFiles, 'Workloads', workloads, null);
   writeGroupedFiles(groupedItemFiles, 'Items');
-  writeGroupedFiles(groupedOtherFiles, 'Other');
+  writeGroupedFiles(groupedOtherFiles, 'Other', null, workloads);
 
   // Close the write stream
   writeStream.end(() => {
